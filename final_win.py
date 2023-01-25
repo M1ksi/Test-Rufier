614c6d25da78409a81d6deae03ba7fcc3f4da973
M1ksi <makskajn86@gmail.com>
2023-01-25 19:01:43 +0300

    New version 0.2

--- final_win.py
+++ final_win.py
@@ -1,1 +1,27 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QLabel, QVBoxLayout, QLineEdit
+from my_app import *
+from instr import *
+app = QApplication([])
+my_win = QWidget()
+my_win.setWindowTitle('Здоровье')
+my_win.move(200, 100)
+my_win.resize(1000, 600)
+
+v_line1 = QVBoxLayout()
+LayoutH1 = QHBoxLayout()
+LayoutH2 = QHBoxLayout()
+
+number1 = QLabel('Индекс Руфье: какой-то там')
+number2 = QLabel('Работоспособность сердца: вы умерли')
+
+LayoutH1.addWidget(number1, alignment = Qt.AlignCenter)
+LayoutH2.addWidget(number2, alignment = Qt.AlignCenter)
+
+v_line1.addLayout(LayoutH1)
+v_line1.addLayout(LayoutH2)
+my_win.setLayout(v_line1)
+
+
+my_win.show()
+app.exec_()
--- my_app.py
+++ my_app.py
@@ -1,1 +1,25 @@
 # напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QLabel, QVBoxLayout
+
+from instr import *
+from second_win import *
+
+class MainWin(QWidget):
+    def __init__():
+        super().__init__()
+        def set_appear(self):
+            self.setWindowTitle(txt_title)
+            self.resize(win_width, win_height)
+            self.move(win_x, win_y)
+        def initUI(self):
+            self.hello_text = QLabel(txt_hello)
+            self.instruction = QLabel(txt_instruction)
+            self.button = QPushButton(txt_next)
+            self.layout = QVBoxLayout()
+            self.hello_text.addWidget(self.layout)
+            self.instruction.addWidget(self.layout)
+            self.button.addWidget(self.layout)
+        def next_click(self):
+            self.hide
+            
